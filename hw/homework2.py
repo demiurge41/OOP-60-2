dd4b7598f1bfcaae59ec81cb42ec980bf0da6655
demiurge41 <zari736988@gmail.com>
2025-11-12 22:34:11 +0600

    рщhomework2

--- hw/homework2.py
+++ hw/homework2.py
@@ -3,6 +3,8 @@
 # где будите хранить код уроков и код ДЗ. Создайте небольшую иерархию классов,
 # моделирующую животных в «волшебном зоопарке». У каждого животного есть базовые
 # характеристики и уникальная суперспособность, которую оно может использовать.
+from codecs import replace_errors
+
 
 # 1) Базовый класс Animal
 # 2) Миксины(mix - in) – отдельные способности, не зависящие от типа животного:
@@ -14,6 +16,53 @@
 # Каждый миксин переопределяет use_ability и вызывает super().use_ability()
 # (чтобысобрать все способностив одну строку).
 #
+
+class Animal:
+    def __init__(self, name: str):
+       self.name = name
+
+    def info(self):
+        return self.name
+
+    def use_ability(self):
+        return "Has no special abilities."
+
+
+"""---"""
+class Flyable:
+    def use_ability(self):
+        return super().use_ability() + " + FLY "
+
+class Swimmable:
+    def use_ability(self):
+        return super().use_ability() + " + SWIMM "
+
+class Invisible:
+    def use_ability(self):
+        return super().use_ability() + " + INVIS "
+
+
+"""---"""
+class Duck(Swimmable, Flyable, Animal):
+    def use_ability(self):
+        return  f"{super().use_ability()} - ( Duck )"
+
+"""---"""
+class Bat(Flyable, Animal):
+    def use_ability(self):
+        return  f"{super().use_ability()} - ( Bat )"
+
+"""---"""
+class Frog(Swimmable, Invisible, Animal):
+    def use_ability(self):
+        return  f"{super().use_ability()} - ( Frog )"
+
+"""---"""
+class Phoenix(Flyable, Animal):
+    def use_ability(self):
+        return  f"{super().use_ability()} - ( Phoenix )"
+
+
 # 3) Конкретные животные (наследуют Animal и нужные миксины):
 # Duck, Bat, Frog, Phoenix
 #
@@ -28,30 +77,27 @@
 
 # 4)
 class Zoo:
-    def __init__(self, animal: str):
-        self.animal = ["Duck, Bat, Frog, Phoenix"]
+    def __init__(self):
+        self.animals = []
 
+    def add_animal(self, animal: Animal):
+        self.animals.append(animal)
 
+    def show_all(self):
+        for animal in self.animals:
+            print(animal.info())
 
+    def perform_show(self):
+        for animal in self.animals:
+            print(f"{animal.name}: {animal.use_ability()}")
 
-class Animal:
-   def __init__(self, name: str, age: int, health: int):
-       self.name = name
-       self.age = age
-       self.health = health      # очки здоровья
+zoo = Zoo()
+zoo.add_animal(Duck("Zigzag McQuack"))
+zoo.add_animal(Bat("Bruce Wayne"))
+zoo.add_animal(Frog("Tsarevna Lyagushka"))
+zoo.add_animal(Phoenix("Skyress Ventus"))
 
-   def info(self) -> str:
-       return f"{self.name}, {self.age} age, HP {self.health}"
-
-   def use_ability(self) -> str:
-       """Переопределяется в наследниках"""
-       return f"{self.name} uses a basic ability."
-
-
-
-
-
-
-
-
-
+print("Animals in ZOO: ")
+zoo.show_all()
+print("Animal show: ")
+zoo.perform_show()
